a5afe3f5bc7e46434c76ed343c4ea1919ff4b80e
oskod <ulkonmogo@gmail.com>
2023-09-18 11:35:23 +0200

    yes

--- fight/js/enemy.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "./sprite.js";
-
-class Enemy extends Sprite {
-	constructor(position) {
-		super(position, 'blue');
-	}
-}
-
-export default Enemy;
--- fight/js/player.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "./sprite.js";
-
-class Player extends Sprite {
-	constructor(position) {
-		super(position, 'red');
-	}
-}
-
-export default Player;
