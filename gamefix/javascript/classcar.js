3b777be918e9b03364e3facbc8d7241417d320d1
Eliah <eliahjkvalvik@gmail.com>
2023-09-22 10:41:32 +0200

    gamefix

---
+++ gamefix/javascript/classcar.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(pos, type, lane) {
+      this.pos = pos;
+      this.type = type;
+      this.lane = lane;
+
+      var element = document.createElement("div");
+      road.appendChild(element);
+      this.element = element;
+    }
+  }
